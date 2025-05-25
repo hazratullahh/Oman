5473c0e0d518acfcd0842f74a5dfe1c56b985c57
ManzoorWayar <manzoorahmadwayar@gmail.com>
2025-05-25 22:27:29 +0430

    add changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,8 +18,8 @@ export function Hero() {
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.8, ease: "easeOut" }}
     >
-      <div className="animation-delay-8 animate-fadeIn pt-1 flex flex-col items-center justify-center px-4 text-center md:pt-20">
-        <div className="z-10 mb-6 mt-5 sm:justify-center md:mb-4 md:mt-2">
+      <div className="animation-delay-8 animate-fadeIn pt-1 flex flex-col items-center justify-center px-4 text-center md:pt-16">
+        <div className="z-10 mb-6 md:mt-5 sm:justify-center md:mb-4">
           {/*
             Uncomment and customize this promo badge if needed:
             <div className="relative flex items-center whitespace-nowrap rounded-full border bg-popover px-3 py-1 text-xs leading-6 text-primary/60">
@@ -41,9 +41,19 @@ export function Hero() {
         </div>
 
         <div className="mb-10 mt-4 md:mt-6">
+
+          <h2 className="mb-7 md:mb-10 text-xl md:text-3xl text-gray-50">
+            Comopmay Name be in there
+          </h2>
+
+          {/* <p className="md:text-md mx-auto mb-16 mt-2 max-w-2xl px-6 text-sm text-gray-50 sm:px-6 md:max-w-4xl md:px-20 lg:text-lg">
+            I craft enchanting visuals for brands, and conjure design resources
+            to empower others.
+          </p> */}
+
           <div className="px-2">
             <div className="border-white relative mx-auto h-full max-w-7xl border p-6 [mask-image:radial-gradient(800rem_96rem_at_center,white,transparent)] md:px-12 md:py-20">
-              <h1 className="flex font-serif select-none flex-col px-3 py-2 text-center text-5xl font-semibold leading-none tracking-tight md:flex-col md:text-8xl lg:flex-row lg:text-8xl text-white">
+              <h1 className="flex font-serif capitalize select-none flex-col px-3 py-2 text-center text-5xl font-semibold leading-none tracking-tight md:flex-col md:text-8xl lg:flex-row lg:text-8xl text-white">
                 <DIcons.Plus
                   strokeWidth={4}
                   className="text-white absolute -left-5 -top-5 h-10 w-10"
@@ -60,9 +70,9 @@ export function Hero() {
                   strokeWidth={4}
                   className="text-white absolute -bottom-5 -right-5 h-10 w-10"
                 />
-                Empowering Your Business with Expert Setup and Investments!
+                Setup your business investment with expert!
               </h1>
-              <div className="flex items-center justify-center gap-1">
+              <div className="flex items-center justify-center gap-1 pt-5">
                 <span className="relative flex h-3 w-3 items-center justify-center">
                   <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-500 opacity-75"></span>
                   <span className="relative inline-flex h-2 w-2 rounded-full bg-green-500"></span>
@@ -71,16 +81,6 @@ export function Hero() {
               </div>
             </div>
           </div>
-
-          <h2 className="mt-8 text-2xl md:text-2xl text-gray-50">
-            Welcome to my creative playground! I’m{" "}
-            <span className="text-white font-bold">Ali</span>
-          </h2>
-
-          <p className="md:text-md mx-auto mb-16 mt-2 max-w-2xl px-6 text-sm text-gray-50 sm:px-6 md:max-w-4xl md:px-20 lg:text-lg">
-            I craft enchanting visuals for brands, and conjure design resources
-            to empower others.
-          </p>
 
           {/*
             Uncomment and customize these buttons if you want CTAs:
--- src/components/ui/WhyChooseSection.jsx
+++ src/components/ui/WhyChooseSection.jsx
@@ -99,7 +99,7 @@ export function FeatureSteps({
                         width={1000}
                         height={500}
                       />
-                      <div className="absolute bottom-0 left-0 right-0 h-2/3 bg-gradient-to-t from-background via-background/50 to-transparent" />
+                      <div className="absolute bottom-0 left-0 right-0 h-2/3 bg-gradient-to-t from-gray-950 via-gray-950/50 to-transparent" />
                     </motion.div>
                   ),
               )}
