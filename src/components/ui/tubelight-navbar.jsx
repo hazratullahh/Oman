0cf822a0aefb0ed81a9a5937fcb7ac4e1a923034
ManzoorWayar <manzoorahmadwayar@gmail.com>
2025-05-27 21:28:30 +0430

    change the shcase

--- src/components/ui/tubelight-navbar.jsx
+++ src/components/ui/tubelight-navbar.jsx
@@ -40,7 +40,7 @@ export function NavBar({ items, className }) {
 
       <div
         className={cn(
-          "fixed bottom-0 sm:top-0 left-1/2 -translate-x-1/2  mb-6 sm:pt-6",
+          "fixed bottom-0 sm:top-0 left-1/2 -translate-x-1/2 z-50 mb-6 sm:pt-6",
           className,
         )}
       >
