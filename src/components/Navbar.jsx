22b796657d8faf3630bcf741722428e0018b0212
jawaderfani01 <jawad.erfani147@gmail.com@gmail.com>
2025-05-28 16:22:43 +0430

    add ScrollToHash component for smooth scrolling and update Navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,18 @@
-"use client"
+// app/auth/page.js
+"use client";
 
-import { Home, User, Contact, FileText, MailQuestion,  } from 'lucide-react'
-import { NavBar } from "@/components/ui/tubelight-navbar"
+import { Home, User, Contact, FileText, MailQuestion } from "lucide-react";
+import { NavBar } from "@/components/ui/tubelight-navbar";
 
 export function NavBarMobile() {
   const navItems = [
-    { name: 'Home', url: '/', icon: Home },
-    { name: 'About', url: '/about', icon: User },
-    { name: 'Services', url: '#services', icon: FileText },
-    { name: 'Contact', url: '/contact', icon: Contact },
-    { name: 'FAQ', url: '/faq', icon: MailQuestion }
-  ]
+    { name: "Home", url: "/", icon: Home },
+    { name: "About", url: "/about", icon: User },
+    // *** CHANGE THIS LINE ***
+    { name: "Services", url: "/#services", icon: FileText }, // <-- Change here
+    { name: "Contact", url: "/contact", icon: Contact },
+    { name: "FAQ", url: "/faq", icon: MailQuestion },
+  ];
 
-  return <NavBar items={navItems} />
+  return <NavBar items={navItems} />;
 }
