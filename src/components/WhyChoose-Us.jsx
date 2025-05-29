4e9f2e33d10c5e53b693bf6adb22548c80beec65
jawaderfani01 <jawad.erfani147@gmail.com@gmail.com>
2025-05-29 10:25:39 +0430

    feat: add images for Why Choose Us section

--- src/components/WhyChoose-Us.jsx
+++ src/components/WhyChoose-Us.jsx
@@ -1,33 +1,36 @@
-import { FeatureSteps } from "@/components/ui/WhyChooseSection"
+import { FeatureSteps } from "@/components/ui/WhyChooseSection";
 
 const features = [
-    {
-        step: 'Step 1',
-        title: 'Expertise – Deep knowledge of Oman regulations',
-        content: 'Our experienced team ensures your business fully complies with all Oman regulations, minimizing risks and avoiding delays.',
-        image: 'https://images.unsplash.com/photo-1723958929247-ef054b525153?q=80&w=2070&auto=format&fit=crop'
-    },
-    {
-        step: 'Step 2',
-        title: 'Efficiency – Fast and hassle-free processing',
-        content: 'We provide a streamlined process using advanced tools to deliver fast, hassle-free approvals and keep your projects moving smoothly.',
-        image: 'https://images.unsplash.com/photo-1723931464622-b7df7c71e380?q=80&w=2070&auto=format&fit=crop'
-    },
-    {
-        step: 'Step 3',
-        title: 'Personalized - Support Dedicated coordinator  for each client',
-        content: 'Every client receives a dedicated coordinator who offers personalized guidance and regular updates throughout the entire process.',
-        image: 'https://images.unsplash.com/photo-1725961476494-efa87ae3106a?q=80&w=2070&auto=format&fit=crop'
-    },
-]
+  {
+    step: "Step 1",
+    title: "Expertise – Deep knowledge of Oman regulations",
+    content:
+      "Our experienced team ensures your business fully complies with all Oman regulations, minimizing risks and avoiding delays.",
+    image: "/images/choose/choose-1.jpg",
+  },
+  {
+    step: "Step 2",
+    title: "Efficiency – Fast and hassle-free processing",
+    content:
+      "We provide a streamlined process using advanced tools to deliver fast, hassle-free approvals and keep your projects moving smoothly.",
+    image: "/images/choose/choose-2.jpg",
+  },
+  {
+    step: "Step 3",
+    title: "Personalized - Support Dedicated coordinator  for each client",
+    content:
+      "Every client receives a dedicated coordinator who offers personalized guidance and regular updates throughout the entire process.",
+    image: "/images/choose/choose-3.jpg",
+  },
+];
 
 export function WhyChooseUS() {
-    return (
-        <FeatureSteps
-            features={features}
-            title="Why Choose Us"
-            autoPlayInterval={4000}
-            imageHeight="h-[500px]"
-        />
-    )
+  return (
+    <FeatureSteps
+      features={features}
+      title="Why Choose Us"
+      autoPlayInterval={4000}
+      imageHeight="h-[500px]"
+    />
+  );
 }
