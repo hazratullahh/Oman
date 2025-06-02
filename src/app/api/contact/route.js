96fb6f15040d9e0e33fb431e8206faf36c9c4e98
ManzoorWayar <manzoorahmadwayar@gmail.com>
2025-06-03 00:46:41 +0430

    add canges to the website

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -5,7 +5,7 @@ export async function POST(req) {
     const data = await req.json();
 
     await sendMail({
-      to: "service@tasheelom.com",
+      to: "support@tasheelom.com",
       from: data.email,
       subject: `Tas-Heel - Received New Contact From ${data.firstName} ${data.lastName}!`,
       html: data.html, // pass the rendered template html here
